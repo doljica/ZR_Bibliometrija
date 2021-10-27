019722b6be550b0dd5a78d5366a1d709887959c5
doljica <55084552+doljica@users.noreply.github.com>
2021-10-27 15:48:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import csv
 csv_file = open('data_1-1000.csv')
 csv_reader = csv.reader(csv_file)
 csv_data = list(csv_reader)
-print(csv_data[3][2])
+
+for row in csv_reader:
+    print('Row #' + str(csv_reader.line_num) + '' + str(row))
