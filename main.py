32f4bbd99335eece80252d187c056fd1c4270469
doljica <55084552+doljica@users.noreply.github.com>
2021-10-27 16:01:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ csv_data = list(csv_reader)
 
 for row in csv_reader:
     print('Row #' + str(csv_reader.line_num) + '' + str(row))
+
+#output_file = open('output.csv', 'w', newline='')
+#output_writer = csv.writer(output_file)
+#output_writer.writerow()
